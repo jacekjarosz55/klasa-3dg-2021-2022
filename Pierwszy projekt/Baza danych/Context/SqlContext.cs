e3e1d97b532b83c5d674381c7b8b85efa60180ef
Łukasz Felisek <felisek1@wp.pl>
2022-03-01 07:43:52 +0100

    koniec lekcji

---
+++ Pierwszy projekt/Baza danych/Context/SqlContext.cs
@@ -0,0 +1,22 @@
+﻿using Baza_danych.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baza_danych.Context
+{
+    public class SqlContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=Obywatele_DB_3Dg;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Pytanie> Pytania { get; set; }
+        public DbSet<Odpowiedz> Odpowiedzi { get; set; }
+
+    }
+}
--- Pierwszy projekt/Baza danych/Repository/DbRepository.cs
+++ Pierwszy projekt/Baza danych/Repository/DbRepository.cs
@@ -1,42 +1,87 @@
-﻿using Baza_danych.Dto;
+﻿using Baza_danych.Context;
+using Baza_danych.Dto;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Baza_danych.Repository
 {
     public class DbRepository : IRepository
     {
+        SqlContext sqlContext;
 
-
-        public void DeleteOdpowiedzi(int idPytanie)
+        public DbRepository()
         {
-            throw new NotImplementedException();
+            sqlContext = new SqlContext();
         }
 
-        public void DeletePytanie(int idPytanie)
+        #region CRUD
+
+        #region C - Create
+
+
+        #endregion
+
+        #region R - Read
+
+        public List<int> ReadPytaniaIdList()
         {
-            throw new NotImplementedException();
+            //select id from Pytania
+            var result = sqlContext.Pytania.Select(p => p.Id).ToList();
+            return result;
         }
 
         public List<PytanieReadDto> ReadListaPytan()
         {
-            throw new NotImplementedException();
-        }
-
-        public List<OdpowiedzReadDto> ReadOdpowiedzi(int idPytania)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<int> ReadPytaniaIdList()
-        {
-            throw new NotImplementedException();
+            List<PytanieReadDto> readPytanieDtos = quizAutomapper.Mapper.Map<List<PytanieReadDto>>(pytaniaLista);
+            return readPytanieDtos;
         }
 
         public PytanieReadDto ReadPytanie(int id)
         {
-            throw new NotImplementedException();
+            Pytanie pytanie = pytaniaLista.FirstOrDefault(p => /*return*/ p.Id == id);
+            if (pytanie == null)
+                throw new Exception("Nie znaleziono pytania o podanym ID");
+
+            PytanieReadDto readPytanieDto = quizAutomapper.Mapper.Map<PytanieReadDto>(pytanie);
+            return readPytanieDto;
         }
+
+        public List<OdpowiedzReadDto> ReadOdpowiedzi(int idPytania)
+        {
+            List<Odpowiedz> odpowiedzi = odpowiedziLista.Where(o => o.IdPytania == idPytania).ToList();
+
+            List<OdpowiedzReadDto> result = quizAutomapper.Mapper.Map<List<OdpowiedzReadDto>>(odpowiedzi);
+
+            return result;
+        }
+
+
+
+        #endregion
+
+        #region U - Update
+
+
+
+        #endregion
+
+        #region D - Delete
+
+        public void DeleteOdpowiedzi(int idPytanie)
+        {
+            odpowiedziLista.RemoveAll(odp => odp.IdPytania == idPytanie);
+        }
+
+        public void DeletePytanie(int idPytanie)
+        {
+            pytaniaLista.RemoveAll(pyt => pyt.Id == idPytanie);
+        }
+
+        #endregion
+
+
+        #endregion
     }
 }
